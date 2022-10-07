00a19ea9428805940e7ef27e308e56db0c740969
Acer <Acer@LAPTOP-FHAM5QBB>
2022-10-07 20:14:58 +0700

    -Hello

---
+++ src/Dao/ThucDonDAO.java
@@ -0,0 +1,79 @@
+package Dao;
+
+import Dao.MainDao;
+import Dao.MainDao;
+import Model.ThucDon;
+import Helper.ConnectDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+
+public class ThucDonDAO extends MainDao<ThucDon, String>{
+
+    @Override
+    public void insert(ThucDon model) {
+        String sql="INSERT INTO MonAn (MaMon, TenMon, DonGia, DonViHoaDon, MaLoaiMon) VALUES (?, ?, ?, ?, ?)";
+        ConnectDatabase.update(sql, model.getTenMon(), model.getGia());
+    }
+
+    @Override
+    public void update(ThucDon model) {
+        String sql="UPDATE MonAn SET MaMon = ? ,TenMon = ?,DonGia = ?, DonViTinh = ?, MaLoaiMon = ? WHERE MaMon=?";
+        ConnectDatabase.update(sql, 
+                model.getTenMon(),model.getGia());
+    }
+
+    @Override
+    public void delete(String maMon) {
+        String sql="DELETE FROM MonAn WHERE MaMon=?";
+        ConnectDatabase.update(sql, maMon);
+    }
+
+    @Override
+    public ThucDon selectById(String TenMon) {
+        String sql="SELECT * FROM MonAn WHERE TenMon=? ";
+        List<ThucDon> list = this.selectBySql(sql, TenMon);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    @Override
+    public List<ThucDon> selectAll() {
+        String sql="select * from MonAn";
+        return this.selectBySql(sql);
+    }
+
+    @Override
+    protected List<ThucDon> selectBySql(String sql, Object... args) {
+       List<ThucDon> list=new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = ConnectDatabase.query(sql, args);
+                while(rs.next()){
+                    ThucDon entity = new ThucDon();
+                    entity.setMaMon(rs.getString("MaMon"));
+                    entity.setTenMon(rs.getString("TenMon"));
+                    entity.setGia(rs.getFloat("DonGia"));
+                    entity.setDonViHoaDon(rs.getString("DonViHoaDon"));
+                    entity.setMaLoaiMon(rs.getString("MaLoaiMon"));
+                    list.add(entity);
+                }
+            } 
+            finally{
+                rs.getStatement().getConnection().close();
+            }
+        } 
+        catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+    
+    public ThucDon selectThitHeo(String maLoaiMon) {
+        String sql="SELECT * FROM MonAn WHERE MaLoaiMon=? ";
+        List<ThucDon> list = this.selectBySql(sql, maLoaiMon);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+}
--- src/Model/ThucDon.java
+++ src/Model/ThucDon.java
@@ -1,5 +1,62 @@
 package Model;
 
 public class ThucDon {
+    private String maMon;
+    private String maLoaiMon;
+    private String tenMon;
+    private float gia;
+    private String donViHoaDon;
+
+    public ThucDon() {
+    }
+
+    public ThucDon(String maMon, String maLoaiMon, String tenMon, float gia, String donViHoaDon) {
+        this.maMon = maMon;
+        this.maLoaiMon = maLoaiMon;
+        this.tenMon = tenMon;
+        this.gia = gia;
+        this.donViHoaDon = donViHoaDon;
+    }
+
+    public String getTenMon() {
+        return tenMon;
+    }
+
+    public void setTenMon(String tenMon) {
+        this.tenMon = tenMon;
+    }
+
+    public float getGia() {
+        return gia;
+    }
+
+    public void setGia(float gia) {
+        this.gia = gia;
+    }
+
+    public String getMaMon() {
+        return maMon;
+    }
+
+    public void setMaMon(String maMon) {
+        this.maMon = maMon;
+    }
+
+    public String getMaLoaiMon() {
+        return maLoaiMon;
+    }
+
+    public void setMaLoaiMon(String maLoaiMon) {
+        this.maLoaiMon = maLoaiMon;
+    }
+
+    public String getDonViHoaDon() {
+        return donViHoaDon;
+    }
+
+    public void setDonViHoaDon(String donViHoaDon) {
+        this.donViHoaDon = donViHoaDon;
+    }
+    
     
 }
